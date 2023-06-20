9ebe1dc0ccac639ef39920b3784b31015fe36e27
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-06-21 00:00:34 +0300

    Initialise mock server when env variables are set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  require("../../mocks");
+}
+
 export const metadata = {
   title: "KanBan - A project management tool for sonic mosaic band",
   description: "A project management tool for sonic mosaic band",
