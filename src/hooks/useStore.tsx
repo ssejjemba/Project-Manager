8638df9fe95748c4ea303cb9bfe15be6e11559c6
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-06-21 00:43:33 +0300

    Add custom hooks for consumers

---
+++ src/hooks/useStore.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { RootStore } from "../stores/root.store";
+import { RootStoreContext } from "../providers/root.provider";
+
+export const useStore = (): RootStore => {
+  const store = useContext(RootStoreContext);
+  if (!store) {
+    throw new Error("useStore must be used within a RootStoreProvider");
+  }
+  return store;
+};
---
+++ src/providers/root.provider.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode, createContext } from "react";
+import { RootStore } from "../stores/root.store";
+
+// Create the context
+export const RootStoreContext = createContext<RootStore | null>(null);
+
+// Provider component
+interface RootStoreProviderProps {
+  children: ReactNode;
+}
+
+// Provider component
+export const RootStoreProvider = ({ children }: RootStoreProviderProps) => {
+  return (
+    <RootStoreContext.Provider value={RootStore.getInstance()}>
+      {children}
+    </RootStoreContext.Provider>
+  );
+};
