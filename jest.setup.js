3bcd9ea1fe3ff329699d058e557b33b3dc80e0cd
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-06-20 23:51:47 +0300

    Use mock server for the tests

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,8 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
